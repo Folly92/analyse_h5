dfd4d6c4b263cb13e336909791b7c315365f7496
Coralie Neubueser <cneubueser@ui-tier1.cr.cnaf.infn.it>
2020-04-01 12:24:18 +0200

    allow to run on leos data using --leos flag

--- python/readH5.py
+++ python/readH5.py
@@ -11,6 +11,7 @@ r.gStyle.SetPadRightMargin(0.2)
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--inputFile', type=str, help='Define patht to data file.')
+parser.add_argument('--data', type=str, choices=['hepd','leos'], required=True, help='Define patht to data file.')
 
 args,_=parser.parse_known_args()
 
@@ -23,16 +24,28 @@ print(list(f.keys()))
 for dset in traverse_datasets(f):
     print(dset, f[dset].shape, f[dset].dtype)
     
-dset1 = f['L_parameter']
-dset_lonlat = f['LonLat']
-dset_en = f['HEPD_ele_energy_table']
-dset_p = f['HEPD_ele_pitch_table']
-dset2 = f['HEPD_ele_energy_pitch']
-dset_time = f['UTCTime']
+parameters = dict([('hepd', ['L_parameter', 'HEPD_ele_energy_table', 'HEPD_ele_pitch_table', 'HEPD_ele_energy_pitch', 'UTCTime']),
+                   ('leos', ['L_parameter', 'Energy_Table_Electron', 'PitchAngle', 'A411', 'UTC_TIME']),
+               ])
+lonlat = dict([('hepd', ['LonLat', 'LonLat'] ),
+               ('leos', ['GEO_LON', 'GEO_LAT'] )
+           ])
+
+dset1 = f[parameters[args.data][0]]
+dset_lon = f[lonlat[args.data][0]]
+dset_lat = f[lonlat[args.data][1]]
+dset_en = f[parameters[args.data][1]]
+dset_p = f[parameters[args.data][2]]
+dset2 = f[parameters[args.data][3]]
+dset_time = f[parameters[args.data][4]]
 
 maxEv = len(dset2)
 print("Events: ", maxEv)
-time_max = int(str(dset_time[0])[-6:-4])*60*60 +  int(str(dset_time[0])[-4:-2])*60 +  int(str(dset_time[0])[-2:])
+
+time_blanc = dset_time[0]
+if args.data=='leos':
+    time_blanc = dset_time[0][0]
+time_max = int(str(time_blanc)[-6:-4])*60*60 +  int(str(time_blanc)[-4:-2])*60 +  int(str(time_blanc)[-2:])
 
 # prepare root output                            
 outRootName = os.path.split(filename)[1].replace("h5","root")
@@ -64,40 +77,76 @@ hist2D=r.TH2D("hist2D","hist2D",18,1,10,9,10,190)
 hist2D_loc=r.TH2D("hist2D_loc","hist2D_loc",360,1,360,180,-90,90)
 
 for iev,ev in enumerate(dset2):
-    time_calc = 60*60*int(str(dset_time[iev])[-6:-4]) + 60*int(str(dset_time[iev])[-4:-2]) + int(str(dset_time[iev])[-2:])
-    time_act = (time_calc-time_max)/60.
-    
-    hist2D_loc.SetBinContent(int(dset_lonlat[iev][0]+180), int(dset_lonlat[iev][1]+90), float(time_act))
-    # loop through energy bins
-    for ie,en in enumerate(ev):
-        # loop through pitch
-        for ip,count in enumerate(en):
-            if iev==1 and ie==0 and ip==0:
-                print("L-value: ", dset1[iev])
-                print("Pitch:   ", dset_p[0][ip])
-                print("Count:   ", count)
-                print("Energy: ", dset_en[0][ie])
-            if count!=0: # and dset1[iev]<=10:
-                oldbin = hist2D.FindBin(dset1[iev], dset_p[0][ip]) 
-                oldCount = hist2D.GetBinContent(oldbin)
-                hist2D.SetBinContent(oldbin, (oldCount+count)/2.)
-                # fill tree
-                L[0] = dset1[iev]
-                P[0] = dset_p[0][ip]
-                C[0] = count
-                E[0] = dset_en[0][ie]
-                T[0] = time_act
-                
-                # if L-value <=5
-                if dset1[iev]<=5:  
-                    # get indices
-                    ind_L = int(dset1[iev]*2)
-                    ind_tot = (ind_L-1)*9 + ip
-                    Clist[ind_tot][0] = count
-                    
-                tree.Fill()
-                
+    # fill tree and histograms for HEPD data
+    if args.data=='hepd':
+        time_calc = 60*60*int(str(dset_time[iev])[-6:-4]) + 60*int(str(dset_time[iev])[-4:-2]) + int(str(dset_time[iev])[-2:])
+        time_act = (time_calc-time_max)/60.
 
+        hist2D_loc.SetBinContent(int(dset_lon[iev][0]+180), int(dset_lat[iev][1]+90), float(time_act))
+        # loop through energy bins
+        for ie,en in enumerate(ev):
+            # loop through pitch
+            for ip,count in enumerate(en):
+                if iev==1 and ie==0 and ip==0:
+                    print("L-value: ", dset1[iev])
+                    print("Pitch:   ", dset_p[0][ip])
+                    print("Count:   ", count)
+                    print("Energy: ", dset_en[0][ie])
+                if count!=0: # and dset1[iev]<=10:
+                    oldbin = hist2D.FindBin(dset1[iev], dset_p[0][ip]) 
+                    oldCount = hist2D.GetBinContent(oldbin)
+                    hist2D.SetBinContent(oldbin, (oldCount+count)/2.)
+                    # fill tree
+                    L[0] = dset1[iev]
+                    P[0] = dset_p[0][ip]
+                    C[0] = count
+                    E[0] = dset_en[0][ie]
+                    T[0] = time_act
+                
+                    # if L-value <=5
+                    if dset1[iev]<=5:  
+                        # get indices
+                        ind_L = int(dset1[iev]*2)
+                        ind_tot = (ind_L-1)*9 + ip
+                        Clist[ind_tot][0] = count
+                    
+                    tree.Fill()
+    
+    # fill tree and histos for LEOS data 
+    elif args.data=='leos':
+        time_calc = 60*60*int(str(dset_time[iev][0])[-6:-4]) + 60*int(str(dset_time[iev][0])[-4:-2]) + int(str(dset_time[iev][0])[-2:])
+        time_act = (time_calc-time_max)/60.
+
+        hist2D_loc.SetBinContent(int(dset_lon[iev]+180), int(dset_lat[iev]+90), float(time_act))
+        # loop through energy bins
+        for ie,en in enumerate(ev):
+            # loop through pitch
+            for ip,count in enumerate(en):
+                if iev==1 and ie==0 and ip==0:
+                    print("L-value: ", dset1[iev][0])
+                    print("Pitch:   ", dset_p[0][ip])
+                    print("Count:   ", count)
+                    print("Energy: ", dset_en[0][ie])
+
+                if count!=0: 
+                    oldbin = hist2D.FindBin(dset1[iev], dset_p[0][ip])
+                    oldCount = hist2D.GetBinContent(oldbin)
+                    hist2D.SetBinContent(oldbin, (oldCount+count)/2.)
+                    # fill tree 
+                    L[0] = dset1[iev]
+                    P[0] = dset_p[0][ip]
+                    C[0] = count
+                    E[0] = dset_en[0][ie]
+                    T[0] = time_act
+
+                    if dset1[iev]<=5:
+                        # get indices
+                        ind_L = int(dset1[iev]*2)
+                        ind_tot = (ind_L-1)*9 + ip
+                        Clist[ind_tot][0] = count
+
+                    tree.Fill()
+                        
 outpdf = os.path.split(filename)[1]
 outpdf = outpdf.replace("h5","pdf")
 outpdf = outpdf.replace("CSES_HEP","map")
--- python/run.py
+++ python/run.py
@@ -3,12 +3,20 @@ from utils import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--numRuns', type=int, default=1, help='Define number of runs to be analysed.')
+parser.add_argument('--hepd', action='store_true', help='Analyse HEPD data.')
+parser.add_argument('--leos', action='store_true', help='Analyse LEOS data.')
 args,_=parser.parse_known_args()
 
 runs = args.numRuns
 os.system('source /opt/exp_software/limadou/set_env_standalone.sh')
 
-for irun,run in enumerate(glob.glob('/storage/gpfs_data/limadou/data/flight_data/L3h5/*.h5')):
+datapath = ''
+if args.hepd:
+    datapath = '/storage/gpfs_data/limadou/data/flight_data/L3h5/'
+elif args.leos:
+    datapath = '/storage/gpfs_data/limadou/data/cses_data/HEPP_LEOS/*HEP_1*'
+    
+for irun,run in enumerate(glob.glob(datapath+'*.h5')):
     if irun>runs:
         break
     outfile = home()+"/root/"+(os.path.split(run)[1]).replace("h5","root")
@@ -18,6 +26,10 @@ for irun,run in enumerate(glob.glob('/storage/gpfs_data/limadou/data/flight_data
         runs=runs+1
     else:        
         cmd='python3 python/readH5.py --inputFile '+str(run)
+        if args.hepd:
+            cmd+=' --data hepd'
+        elif args.leos:
+            cmd+=' --data leos'
         print(cmd)
         os.system(cmd)
  
